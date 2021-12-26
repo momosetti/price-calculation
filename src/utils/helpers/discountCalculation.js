8ef8db8b5efe0a140e8842ac126b226af15904ab
Momo Setti <zinasetti@gmail.com>
2021-12-26 04:41:15 +0100

    refactor: change utils directories

--- src/utils/helpers/discountCalculation.js
+++ src/utils/helpers/discountCalculation.js
@@ -1,8 +1,8 @@
 /**
  * Function to calculate the milk discount and billed price after applying the offer rule
- * @param {number} ItemQuantity quantity item from the basket
- * @param {number} unitPrice product unit price
- * @returns {Object} Object contains the discountPrice and the billed price depend to the item quantity
+ * @param {number} ItemQuantity quantity item from the basket.
+ * @param {number} unitPrice product unit price.
+ * @returns {Object} Object contains the discountPrice and the billed price depends to the item quantity.
  */
 export const milkDiscountCalculation = (ItemQuantity, unitPrice) => {
   let i = ItemQuantity;
@@ -19,9 +19,9 @@ export const milkDiscountCalculation = (ItemQuantity, unitPrice) => {
 
 /**
  * Calculate the related discount and total price depend on another item (in our case, the butter is the item) quantity
- * @param {number} butterItemQuantity butter items number on the basket
- * @param {number} beardItemQuantity beard items number on the basket
- * @param {number} unitPrice unit price of the beard product
+ * @param {number} butterItemQuantity butter items quantity on the basket.
+ * @param {number} beardItemQuantity beard items quantity on the basket.
+ * @param {number} unitPrice unit price of the beard product.
  * @returns {Object} return an object with discountPrice and paidTotalPrice keys.
  */
 export const beardDiscountCalculation = (
@@ -48,15 +48,15 @@ export const beardDiscountCalculation = (
 };
 
 /**
- * Calculate the statement on the basket
- * @param {Object[]} itemArray array of object added to the basket
- * @returns Object with subTotal, discount, and total keys
+ * Calculate the statement on the basket.
+ * @param {Object[]} itemsArray array of object added to the basket.
+ * @returns Object with subTotal, discount, and total keys.
  */
-export const calculateStatememnt = (itemArray) => {
+export const calculateStatememnt = (itemsArray) => {
   let subTotal = 0;
   let discount = 0;
   let total = 0;
-  itemArray.forEach((item) => {
+  itemsArray.forEach((item) => {
     subTotal = subTotal + item.price * item.quantity;
     discount = discount + item.discount;
     total = subTotal - discount;
